e7795b2cce9aecf99d61f1d510a807a01c067a54
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-07 20:39:01 +0530

    Update SongList.js

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions"; // import the selectSong action creator
+
 function SongList(props) {
-    const { songs } = props;
+    const { songs, selectSong } = props;
+
+    const onSongSelect = (song) => {
+        selectSong(song);
+    };
+
     return (
         <div>
             {songs.map((song) => (
-                <div className="song">{song.title}</div>
+                <div className="song" key={song.title}>
+                    <div>{song.title}</div>
+                    <div>{song.duration}</div>
+                    <button onClick={() => onSongSelect(song)}>Select</button>
+                </div>
             ))}
         </div>
     );
@@ -15,4 +26,23 @@ const mapStateToProps = (state) => {
     return { songs: state.songs };
 };
 
-// Add code here
+export default connect(mapStateToProps, { selectSong })(SongList); // connect the SongList component to the redux store and pass the selectSong action creator as a prop
+
+// import React from "react";
+// import { connect } from "react-redux";
+// function SongList(props) {
+//     const { songs } = props;
+//     return (
+//         <div>
+//             {songs.map((song) => (
+//                 <div className="song">{song.title}</div>
+//             ))}
+//         </div>
+//     );
+// }
+
+// const mapStateToProps = (state) => {
+//     return { songs: state.songs };
+// };
+
+// // Add code here
